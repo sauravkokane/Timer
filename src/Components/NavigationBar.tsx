906d31910cda87fc26748af44ae819e33b342582
Saurav Kokane <saurav.kokane@expleogroup.com>
2023-11-23 12:51:44 +0530

    changed styling of clokc and navbar elements

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -6,18 +6,20 @@ const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#282c34' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
+    paddingInline: theme.spacing(2),
     textAlign: 'center',
-    color: theme.palette.mode === 'dark' ? '#fff' : '#282c34',
+    color: theme.palette.mode === 'dark' ? '#f2f2f2' : '#282c34',
     cursor: 'pointer',
     '&:hover': {
         backgroundColor: theme.palette.mode === 'dark' ? '#f2f2f2' : '#282c34',
-        color: '#f2f2f2',
-    }
+        color: theme.palette.mode === 'dark' ? '#282c34' : '#f2f2f2',
+    },
+    fontWeight: 'bold',
+    fontSize: '30px',
 }));
 const NavigationBar = ({ setNavbarSelection }) => {
     return (
         <div className='nav-bar'>
-            <p>Navigation bar for Timer</p>
             <Stack direction={"row"} spacing={8}>
                 <Item onClick={() => setNavbarSelection('Timer')}>Timer</Item>
                 <Item onClick={() => setNavbarSelection('Clock')}>Clock</Item>
